d50640b50606aebdf672358964f5d768c6434afd
lowderd <duncan.lowder@gmail.com>
2016-01-17 17:04:28 -0700

    Localization and Timezone additions

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -1,12 +1,18 @@
+# -*- coding: utf-8 -*-
 from selenium import webdriver
 from django.core.urlresolvers import reverse
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.utils.translation import activate
+
+from datetime import date
+from django.utils import formats
 
 
 class HomeNewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         # Basic setup for test case
+        activate('en')
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
@@ -33,3 +39,30 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn("Not Found", self.browser.title)
         self.browser.get(self.live_server_url + "/humans.txt")
         self.assertNotIn("Not Found", self.browser.title)
+
+    def test_internationalization(self):
+        for lang, h1_text in [('en', 'Welcome to TaskBuster!'),
+                              ('ca', 'Benvingut a TaskBuster!')]:
+            activate(lang)
+            self.browser.get(self.get_full_url("home"))
+            h1 = self.browser.find_elements_by_tag_name("h1")[1]
+            self.assertEqual(h1.text, h1_text)
+
+    def test_localization(self):
+        today = date.today()
+        for lang in ['en', 'ca']:
+            activate(lang)
+            self.browser.get(self.get_full_url("home"))
+            local_date = self.browser.find_element_by_id("local-date")
+            non_local_date = self.browser.find_element_by_id("non-local-date")
+            self.assertEqual(formats.date_format(today, use_l10n=True), local_date.text)
+            self.assertEqual(today.strftime('%Y-%m-%d'), non_local_date.text)
+
+    def test_timezone(self):
+        self.browser.get(self.get_full_url("home"))
+        tz = self.browser.find_element_by_id("time-tz").text
+        utc = self.browser.find_element_by_id("time-utc").text
+        ny = self.browser.find_element_by_id("time-ny").text
+
+        self.assertNotEqual(tz, utc)
+        self.assertNotEqual(ny, [tz, utc])
--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Django settings for taskbuster project.
 
@@ -13,6 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from django.core.exceptions import ImproperlyConfigured
+from django.utils.translation import ugettext_lazy as _
 
 
 def get_env_variable(var_name):
@@ -47,6 +49,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -67,6 +70,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.i18n',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -91,15 +95,22 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
+LANGUAGES = (
+    ('en', _('English')),
+    ('ca', _('Catalan')),
+)
+
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Mountain'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'),)
 
 
 # Static files (CSS, JavaScript, Images)
